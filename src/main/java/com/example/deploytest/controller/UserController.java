601051975c775c8fb88e33f83ec720d654254f2b
yoooyeon <yooyeon.dev@gmail.com>
2024-06-23 19:00:04 +0900

    controller test

--- src/main/java/com/example/deploytest/controller/UserController.java
+++ src/main/java/com/example/deploytest/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.deploytest.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("users")
+@RestController
+@RequestMapping("users")
 public class UserController {
     @GetMapping
     public String users() {
